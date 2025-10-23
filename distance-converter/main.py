2b9fbba1a1d1479d48a4e524cc404607acc536c3
lapointek <lapointek.dev@gmail.com>
2025-10-23 12:25:03 -0400

    feat(distance-converter): add new button
    feat: add grid functions for setting position of gui

--- distance-converter/main.py
+++ distance-converter/main.py
@@ -1,28 +1,33 @@
 from tkinter import *
 
-window = Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
-
-# Label
-my_label = Label(text="I Am a Label", font=("Arial", 20, "bold"))
-my_label.pack()
-
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
-
 
 def button_clicked():
     new_text = input.get()
     my_label.config(text=new_text)
 
 
+window = Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 20, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+
+# Button
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
-input = Entry()
-input.pack()
+# New Button
+new_button = Button(text="Click Me", command=button_clicked)
+new_button.grid(column=2, row=0)
 
+# Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
 
-# required to run gui
+# Required to run gui
 window.mainloop()
