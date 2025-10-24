e8659f1228d4208c1665bbd26a70a6e430c4bd59
lapointek <lapointek.dev@gmail.com>
2025-10-24 13:47:58 -0400

    feat(pomodoro-start): add canvas and image

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -20,5 +20,9 @@ LONG_BREAK_MIN = 20
 window = Tk()
 window.title("Pomodoro")
 
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
 
 window.mainloop()
