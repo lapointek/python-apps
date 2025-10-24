8be297ecc1e1784455bf6ebea55a48f7761687cd
lapointek <lapointek.dev@gmail.com>
2025-10-24 14:45:29 -0400

    feat: add timer text and image in the center
    feat: add yellow background color

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -5,7 +5,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Roboto"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -19,10 +19,12 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 canvas = Canvas(width=200, height=224)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
+canvas.create_image(101, 112, image=tomato_img)
+canvas.create_text(101, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.pack()
 
 window.mainloop()
