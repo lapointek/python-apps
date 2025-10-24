eaa36462ac7d0326afa93e37794e6a651a69756e
lapointek <lapointek.dev@gmail.com>
2025-10-24 19:18:33 -0400

    feat: add buttons, checkmark, and timer text

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -5,7 +5,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Roboto"
+FONT_NAME = "Noto Sans"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -21,10 +21,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+button_start = Button(text="Start")
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text="Reset")
+button_reset.grid(column=2, row=2)
+
+check_mark = Label(text="check")
+check_mark.grid(column=1, row=3)
+
+timer_text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40))
+timer_text.grid(column=1, row=0)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 window.mainloop()
