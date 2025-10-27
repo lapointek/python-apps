ffd221d91a457aad8bdf2f108f4e355a8401fb54
lapointek <lapointek.dev@gmail.com>
2025-10-27 14:20:09 -0400

    feat: add countdown timer and start timer functions

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -14,14 +14,26 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    count_down(5)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-button_start = Button(text="Start")
+button_start = Button(text="Start", command=start_timer)
 button_start.grid(column=0, row=2)
 
 button_reset = Button(text="Reset")
@@ -36,7 +48,9 @@ timer_text.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold")
+)
 canvas.grid(column=1, row=1)
 
 window.mainloop()
