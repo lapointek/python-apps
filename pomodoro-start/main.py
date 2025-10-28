6891dadc2832541c40b7efc22e7d05426f1e6dd7
lapointek <lapointek.dev@gmail.com>
2025-10-28 13:56:06 -0400

    feat(pomodoro-start): add conditional for checkmark to be added after timer ends feat: add conditionals to change text color depending on timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -2,14 +2,18 @@ from tkinter import *
 import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
+
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Noto Sans"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -17,7 +21,22 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -26,10 +45,19 @@ def start_timer():
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -43,11 +71,11 @@ button_start.grid(column=0, row=2)
 button_reset = Button(text="Reset")
 button_reset.grid(column=2, row=2)
 
-check_mark = Label(text="✔", fg=GREEN, bg=YELLOW)
-check_mark.grid(column=1, row=3)
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
-timer_text = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40))
-timer_text.grid(column=1, row=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40))
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
