d938ec1c8817022247e3833cc3b8e6214d4dfbeb
lapointek <lapointek.dev@gmail.com>
2025-10-29 12:45:48 -0400

    feat: add label and text input for website, email, and password

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -10,9 +10,24 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 window.mainloop()
