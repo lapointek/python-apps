5b9fdb9da2a304d62d20ff84bf6f0e387c77def8
lapointek <lapointek.dev@gmail.com>
2025-10-29 16:26:13 -0400

    feat: add save function to save entry values to a file

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,15 +1,28 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.txt", "a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+    website_entry.delete(0, "end")
+    password_entry.delete(0, "end")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+# window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
+# canvas
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_image)
@@ -36,7 +49,7 @@ password_entry.grid(row=3, column=1)
 # buttons
 generate_password_button = Button(text="Generate")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
