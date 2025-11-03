48a9e370ec202723a6c7588edbe7136a8422fe82
lapointek <lapointek.dev@gmail.com>
2025-11-03 11:58:48 -0500

    feat: update logic to save data to a json file

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -89,6 +90,12 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(
@@ -100,8 +107,16 @@ def save():
             message=f"These are the details entered:\nEmail: {email}\nPassword: {password}\nIs it ok the save?",
         )
         if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website} | {email} | {password}\n")
+            with open("data.json", "r") as file:
+                # Reading old data
+                data = json.load(file)
+                # Updating old data with new data
+                data.update(new_data)
+
+            with open("data.json", "w") as file:
+                # Saving updated data
+                json.dump(data, file, indent=4)
+
             website_entry.delete(0, "end")
             password_entry.delete(0, "end")
 
