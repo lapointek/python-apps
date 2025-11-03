19011b321c500eca47a10a5691766328ec72e776
lapointek <lapointek.dev@gmail.com>
2025-11-03 15:12:09 -0500

    feat: add exception handling to the find_password function

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -130,13 +130,21 @@ def save():
 # ---------------------------- FIND PASSWORD ------------------------------- #
 def find_password():
     website = website_entry.get()
-    with open("data.json") as file:
-        data = json.load(file)
+    try:
+        with open("data.json") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
         if website in data:
             email = data[website]["email"]
             password = data[website]["password"]
             messagebox.showinfo(
                 title=website, message=f"Email: {email}\nPassword: {password}"
+            )
+        else:
+            messagebox.showinfo(
+                title="Error", message=f"No details for {website} exists."
             )
 
 
