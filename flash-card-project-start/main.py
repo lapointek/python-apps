1f0cb1a5ab45d8f030900b789caeb0a4624893fe
lapointek <lapointek.dev@gmail.com>
2025-11-04 16:51:10 -0500

    feat: add flip_card function to display card background image

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -6,18 +6,27 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=current_card["French"])
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=back_image)
 
 
 # window
 window = Tk()
 window.title("Flashy")
 window.config(padx=100, pady=50, bg=BACKGROUND_COLOR)
+window.after(3000, func=flip_card)
 
 # images
 front_image = PhotoImage(file="images/card_front.png")
@@ -27,11 +36,13 @@ wrong_image = PhotoImage(file="images/wrong.png")
 
 # canvas
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=front_image)
-card_word = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
-card_title = canvas.create_text(400, 263, text="", font=("Ariel", 40, "italic"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
+
+# text
+card_background = canvas.create_image(400, 263, image=front_image)
+card_word = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(400, 263, text="", font=("Ariel", 40, "italic"))
 
 # buttons
 button_right = Button(image=right_image, highlightthickness=0, command=next_card)
