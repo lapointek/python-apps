156f6bd05e064af9a9e5036205260c9748c0dc6b
lapointek <lapointek.dev@gmail.com>
2025-11-04 16:12:34 -0500

    feat: add next_card function for randomly displaying words refactor: add variable names to create_text

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pd.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 # window
 window = Tk()
@@ -16,16 +28,17 @@ wrong_image = PhotoImage(file="images/wrong.png")
 # canvas
 canvas = Canvas(width=800, height=526)
 canvas.create_image(400, 263, image=front_image)
-canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_word = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_title = canvas.create_text(400, 263, text="", font=("Ariel", 40, "italic"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # buttons
-button_right = Button(image=right_image, highlightthickness=0)
+button_right = Button(image=right_image, highlightthickness=0, command=next_card)
 button_right.grid(column=0, row=1)
-button_wrong = Button(image=wrong_image, highlightthickness=0)
+button_wrong = Button(image=wrong_image, highlightthickness=0, command=next_card)
 button_wrong.grid(column=1, row=1)
 
+next_card()
 
 window.mainloop()
