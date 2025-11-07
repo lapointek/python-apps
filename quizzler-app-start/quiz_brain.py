37b1564932206e2980a9ea381380c878854c194e
lapointek <lapointek.dev@gmail.com>
2025-11-07 14:47:30 -0500

    feat: add QuizInterface class

--- quizzler-app-start/quiz_brain.py
+++ quizzler-app-start/quiz_brain.py
@@ -15,9 +15,10 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(self.current_question.txt)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
+        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
+        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
@@ -29,3 +30,6 @@ class QuizBrain:
 
         print(f"Your current score is: {self.score}/{self.question_number}")
         print("\n")
+
+    def get_next_question(self):
+        self.quiz.next_question
