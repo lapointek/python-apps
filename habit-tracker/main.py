c754c84fbc0883b3548f4427e76ac13c83f09527
lapointek <lapointek.dev@gmail.com>
2025-11-19 14:17:28 -0500

    feat: add post request to display pixel on graph

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -3,6 +3,7 @@ import os
 
 USERNAME = os.environ.get("HT_USERNAME")
 TOKEN = os.environ.get("HT_TOKEN")
+GRAPH_ID = "graph1"
 
 
 pixela_endpoint = "https://pixe.la/v1/users"
@@ -19,7 +20,7 @@ user_params = {
 
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Running Graph",
     "unit": "Km",
     "type": "float",
@@ -33,6 +34,15 @@ headers = {"X-USER-TOKEN": TOKEN}
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20251119",
+    "quantity": "3.00",
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
